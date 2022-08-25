1084799eec3e7c2c3baf42b4bd572b8c1e5ee8dc
nnnkkk7 <kurodanaoki0711pana@gmail.com>
2022-08-25 15:05:18 +0900

    add binarysearch

---
+++ code6.1/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+var a = []int{3, 5, 8, 10, 14, 17, 21, 39}
+var n int = 8
+
+func main() {
+	fmt.Println(binarySearch(10))
+	fmt.Println(binarySearch(39))
+	fmt.Println(binarySearch(100))
+
+}
+
+func binarySearch(key int) int {
+	left := 0
+	right := len(a) - 1
+	for right >= left {
+		mid := left + (right - left)
+		if a[mid] == key {
+			return mid
+		} else if a[mid] > key {
+			right = mid - 1
+		} else if a[mid] < key {
+			left = mid + 1
+		}
+	}
+	return -1
+}
